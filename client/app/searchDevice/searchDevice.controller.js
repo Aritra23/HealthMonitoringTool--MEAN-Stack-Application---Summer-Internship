455cce6d39ea4e754c04d61dd27684093b1d8b21
Dilip <sndilip17@gmail.com>
2015-06-18 18:00:40 -0700

    changed to new workflow - Prof feedback impl

--- client/app/searchDevice/searchDevice.controller.js
+++ client/app/searchDevice/searchDevice.controller.js
@@ -4,7 +4,7 @@
 (function () {
     'use strict';
     angular.module('app.ui.form').controller('searchDeviceCtrl', [
-        '$scope', '$location', '$rootScope', '$route', '$document','filterFilter', function($scope, $location, $rootScope, $route, $document,filterFilter) {
+        '$scope', '$location', '$rootScope', '$route', '$document','filterFilter','logger', function($scope, $location, $rootScope, $route, $document,filterFilter,logger) {
 
             //scope variable initialization
             $scope.unusualList = [];
@@ -24,17 +24,24 @@
             $scope.riskSelection = [];
             $scope.featureSelection = [];
 
+            //other risk factors
+            $scope.otherRisks = [];
+
             //helper function to convert array to JSON format for checkbox implementation
             var convertArrToJson = function(arr){
+                var newarr = [];
                 for(var val in arr){
-                    arr[val] = {name : arr[val], selected : "false"};
+                    newarr[val] = {name : arr[val], selected : "false"};
                 }
-                return arr;
+                return newarr;
             };
 
             // hardcoding risk factors
             var rf = ["Weight", "BMI", "Pulse", "Blood Pressure", "Oxygen Saturation", "Activity"];
             $scope.riskFactors = convertArrToJson(rf);
+
+            var rfAll = ["Weight", "BMI", "Pulse", "Blood Pressure", "Oxygen Saturation", "Exercise/Activity", "Forced Expiratory Volume", "Peak Expiratory Flow", "Ventilation-Perfusion Ratio","Tobacco Cessation", "Heart Rate","Heart Rhythm", "Blood Glucose","HbA1c"];
+
 
             // hardcoding device features
             var df = ["Cost", "Smartphone Connectivity", "BP", "ECG", "HR", "SpO2", "Resp. Rate", "Steps", "Distance", "Calories", "Position", "Sleep", "Glucose", "EEG", "GSR", "Weight", "Fat %", "BMI", "Hydration", "Temperature"];
@@ -65,6 +72,11 @@
                     return item.name;
                 });
             }, true);
+            $scope.$watch('otherRisks|filter:{selected:true}', function (nv) {
+                $scope.riskSelection = nv.map(function (item) {
+                    return item.name;
+                });
+            }, true);
             $scope.$watch('deviceFeatures|filter:{selected:true}', function (nv) {
                 $scope.featureSelection = nv.map(function (item) {
                     return item.name;
@@ -74,30 +86,30 @@
             $scope.isValidateStep1 = function() {
 
                 if($scope.diseaseSelection.length == 0){
+                    //making default selection to be HF
                     $scope.diseaseSelection = ["Heart Failure"];
                 }
 
                 //pull from database risk factors for each disease using a service call or find an optimization
                 if($scope.diseaseSelection.indexOf("Heart Failure") != -1){
                     rf = ["Weight", "BMI", "Pulse", "Blood Pressure", "Oxygen Saturation", "Activity"];
-                    //rf.concat(["Weight", "BMI", "Pulse", "Blood Pressure", "Oxygen Saturation", "Activity"]);
                     $scope.riskFactors = convertArrToJson(rf);
-
+                    $scope.otherRisks = convertArrToJson(_.difference(rfAll,rf));
                 }
                 if($scope.diseaseSelection.indexOf("COPD") != -1){
-                    //rf.concat(["Forced Expiratory Volume", "Peak Expiratory Flow", "Pulse", "Blood Pressure", "Oxygen Saturation", "Activity"]);
                     rf = ["Forced Expiratory Volume", "Peak Expiratory Flow", "Ventilation-Perfusion Ratio","Oxygen Saturation","Tobacco Cessation","Exercise/Activity"];
                     $scope.riskFactors = convertArrToJson(rf);
+                    $scope.otherRisks = convertArrToJson(_.difference(rfAll,rf));
                 }
                 if($scope.diseaseSelection.indexOf("Atrial Fibrillation") != -1){
-                    //rf.concat(["Forced Expiratory Volume", "Peak Expiratory Flow", "Pulse", "Blood Pressure", "Oxygen Saturation", "Activity"]);
                     rf = ["Heart Rate","Heart Rhythm"];
                     $scope.riskFactors = convertArrToJson(rf);
+                    $scope.otherRisks = convertArrToJson(_.difference(rfAll,rf));
                 }
-                if($scope.diseaseSelection.indexOf("Atrial Fibrillation") != -1){
-                    //rf.concat(["Forced Expiratory Volume", "Peak Expiratory Flow", "Pulse", "Blood Pressure", "Oxygen Saturation", "Activity"]);
+                if($scope.diseaseSelection.indexOf("Diabetes") != -1){
                     rf = ["Blood Glucose","Weight","Activity/Exercise","HbA1c"];
                     $scope.riskFactors = convertArrToJson(rf);
+                    $scope.otherRisks = convertArrToJson(_.difference(rfAll,rf));
                 }
             };
 
@@ -119,11 +131,21 @@
 
             };
 
+            $scope.notify = function(type) {
+                switch (type) {
+                    case 'success':
+                        return logger.logSuccess("Thank You. Subscription has been made to the patient");
+                    case 'warning':
+                        return logger.logWarning("Warning!");
+                    case 'error':
+                        return logger.logError("Oh snap! Change a few things up and try submitting again.");
+                }
+            };
+
             return $scope.finishedWizard = function() {
                 //find device call
                 alert("Thank You. Subscription has been made to the patient");
             };
-
             }
     ]);
 })();
