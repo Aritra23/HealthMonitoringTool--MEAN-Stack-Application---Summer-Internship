4cdd386728668579a26c6bd66401a811f50d0f54
Aritra23 <aklahiri@asu.edu>
2015-06-25 15:11:34 -0700

    Made slight modifications in Devices page as suggested by Professor

--- client/app/searchDevice/searchDevice.controller.js
+++ client/app/searchDevice/searchDevice.controller.js
@@ -40,10 +40,10 @@
             //hardcoding patient characteristics
             $scope.patientChar = ["visual impairment", "hearing impairment", "fine motor dexterity", "cognitive impairment", "caregiver", "neuropathy"];
             // hardcoding risk factors
-            var rf = ["Weight", "BMI", "Pulse", "Blood Pressure", "Oxygen Saturation", "Activity"];
+            var rf = ["Weight", "BMI", "Resting HR", "Blood Pressure", "Oxygen Saturation", "Activity"];
             $scope.riskFactors = convertArrToJson(rf);
 
-            var rfAll = ["Weight", "BMI", "Pulse", "Blood Pressure", "Oxygen Saturation", "Exercise/Activity", "Forced Expiratory Volume", "Peak Expiratory Flow", "Ventilation-Perfusion Ratio","Tobacco Cessation", "Heart Rate","Heart Rhythm", "Blood Glucose","HbA1c"];
+            var rfAll = ["Weight", "BMI", "Resting HR", "Blood Pressure", "Oxygen Saturation", "Exercise/Activity", "Forced Expiratory Volume", "Peak Expiratory Flow", "Ventilation-Perfusion Ratio","Tobacco Cessation", "Heart Rate","Heart Rhythm", "Blood Glucose","HbA1c"];
 
 
             // hardcoding device features
@@ -99,7 +99,7 @@
 
                 //pull from database risk factors for each disease using a service call or find an optimization
                 if($scope.diseaseSelection.indexOf("Heart Failure") != -1){
-                    rf = ["Weight", "BMI", "Pulse", "Blood Pressure", "Oxygen Saturation", "Activity"];
+                    rf = ["Weight", "BMI", "Resting HR", "Blood Pressure", "Oxygen Saturation", "Activity"];
                     $scope.riskFactors = convertArrToJson(rf);
                     $scope.otherRisks = convertArrToJson(_.difference(rfAll,rf));
 
