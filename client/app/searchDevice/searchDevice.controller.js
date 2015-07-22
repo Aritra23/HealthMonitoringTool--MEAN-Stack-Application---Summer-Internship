240628f3a4b4427688208970ad68890923522aaf
Aritra23 <aklahiri@asu.edu>
2015-07-22 10:46:07 -0700

    All changes committed

--- client/app/searchDevice/searchDevice.controller.js
+++ client/app/searchDevice/searchDevice.controller.js
@@ -2,7 +2,7 @@
  * Created by Dilip on 6/2/2015.
  */
 (function () {
-<<<<<<< HEAD
+
     //'use strict';
     angular.module('app.ui').factory('searchDeviceFactory', ['$http', function ($http) {
             var baseUrl = "http://localhost:9000/mongoapi";
@@ -63,84 +63,28 @@
                 getAllDiseases();
 
                 // watch the scope variables set from MongoDB call
-                $scope.$watch('test',function(newValue, oldValue){
+                $scope.$watch('test',function(newValue, oldValue) {
                     console.log("Inside test watch New Value ::" + newValue + "Old Value :::" + oldValue);
-                    if(newValue) {
+                    if (newValue) {
 
                         //$scope.diseaseList.push(newValue);
-                        for(var item in $scope.test) {
+                        for (var item in $scope.test) {
                             // $scope.diseaseList.push({name: $scope.diseaseList[item].name, selected:"false", imgsrc: ""});
                             //$scope.diseaseList = $scope.test.Object.name;
                             console.log($scope.test[item]);
 
                             $scope.diseaseList.push({name: $scope.test[item].name, selected: "false", imgsrc: ""});
-                            for(var temp in $scope.diseaseList){
-                                $scope.diseaseList[temp].imgsrc = "../images/disease-images/"+$scope.diseaseList[temp].name.replaceAll(" ","")+".png";
+                            for (var temp in $scope.diseaseList) {
+                                $scope.diseaseList[temp].imgsrc = "../images/disease-images/" + $scope.diseaseList[temp].name.replaceAll(" ", "") + ".png";
                             }
-
 
 
                         }
                         console.log("DL:", $scope.diseaseList);
                         console.log("Value is set");
                     }
-=======
-    'use strict';
-    angular.module('app.ui').factory('searchDeviceFactory', ['$http','$q',function ($http,$q) {
-        var baseUrl = "http://localhost:9000/mongoapi";
-            return {
-            getAllFeatures : function(){
-                return $http({
-                    url: baseUrl + '/getAllFeatures',
-                    method : 'GET'
-                }).then(
-                    function(payload) {
-                        console.log("Response inside promise ::::",payload.data);
-                        return payload.data;
-                    });
-          },
-            getAllDiseases :function(){
-                return $http({
-                    url: baseUrl + '/getAllDiseases',
-                    method : 'GET'
-                }).then(
-                    function(payload) {
-                        console.log("Response inside promise ::::",payload.data);
-                        return payload.data;
-                    });
-            }
-        }
-    }]
-    )
-    .controller('searchDeviceCtrl', [
-        '$scope', '$location', '$rootScope', '$route', '$document','filterFilter','logger','WizardHandler','$timeout','searchDeviceFactory','$q', function($scope, $location, $rootScope, $route, $document,filterFilter,logger,WizardHandler,$timeout,searchDeviceFactory,$q) {
-
-            //scope variable initialization
-            $scope.unusualList = [];
-            $scope.wizard = {
-                symptom: '',
-                inpError : false
-            };
-            $scope.test = [];
-
-            //testing factory
-                var getAllDiseases = function() {
-                    searchDeviceFactory.getAllDiseases()
-                        .then(function(data) {
-                            $scope.test = data;
-                        });
-                };
-                getAllDiseases();
-
-                // watch the scope variables set from MongoDB call
-                $scope.$watch('test',function(newValue, oldValue){
-                    console.log("Inside test watch New Value ::" + newValue + "Old Value :::" + oldValue);
                 },true);
 
->>>>>>> 9e0fef27fa6f7ab2c9477c8a67df756e025f211c
-
-
-                },true);
 
 
                 $scope.diseaseSelection = [];
@@ -287,9 +231,9 @@
                  });*/
 
                 //assign value to imgsrc
-                for (var item in $scope.diseaseList) {
+              /*  for (var item in $scope.diseaseList) {
                     $scope.diseaseList[item].imgsrc = "../images/disease-images/" + $scope.diseaseList[item].name.replaceAll(" ", "") + ".png";
-                }
+                }*/
 
                 //selection list
                 //$scope.diseaseSelection = [];
@@ -300,10 +244,10 @@
                 $scope.otherRisks = [];
 
                 //helper function to convert array to JSON format for checkbox implementation
-                var convertArrToJson = function (arr) {
+                var convertArrToJson = function(arr){
                     var newarr = [];
-                    for (var val in arr) {
-                        newarr[val] = {name: arr[val], selected: "false"};
+                    for(var val in arr){
+                        newarr[val] = {name : arr[val], selected : "false"};
                     }
                     return newarr;
                 };
@@ -316,78 +260,33 @@
                 var rf = ["Weight", "BMI", "Resting Heart Rate", "Blood Pressure", "Oxygen Saturation", "Activity"];
                 $scope.riskFactors = convertArrToJson(rf);
 
-                var rfAll = ["Weight", "BMI", "Resting Heart Rate", "Blood Pressure", "Oxygen Saturation", "Activity", "Forced Expiratory Volume", "Peak Expiratory Flow", "Ventilation-Perfusion Ratio", "Tobacco Cessation", "Heart Rate", "Heart Rhythm", "Blood Glucose", "HbA1c"];
+                var rfAll = ["Weight", "BMI", "Resting Heart Rate", "Blood Pressure", "Oxygen Saturation", "Activity", "Forced Expiratory Volume", "Peak Expiratory Flow", "Ventilation-Perfusion Ratio","Tobacco Cessation", "Heart Rate","Heart Rhythm", "Blood Glucose","HbA1c"];
 
-<<<<<<< HEAD
                 // hardcoding device features
                 var df = ["Cost", "Smartphone Connectivity", "BP", "ECG", "HR", "SpO2", "Resp. Rate", "Steps", "Distance", "Calories", "Position", "Sleep", "Glucose", "EEG", "GSR", "Weight", "Fat %", "BMI", "Hydration", "Temperature"];
                 $scope.deviceFeatures = convertArrToJson(df);
 
                 //hardcoding device categories
-                $scope.deviceCategory = [{name: "Body Composition Analyzer", imgsrc: ""},
-                    {name: "Blood Pressure Monitor", imgsrc: ""},
-                    {name: "Activity Tracker", imgsrc: ""},
-                    {name: "Heart Rate Monitor", imgsrc: ""},
-                    {name: "Oximeter", imgsrc: ""}];
-=======
-            //filter selection
-            $scope.selectedDiseases = function selectedDiseases() {
-                return filterFilter($scope.diseaseList, { selected: true });
-            };
-            $scope.selectedRisks = function selectedRisks(){
-                return filterFilter($scope.riskFactors, { selected: true });
-            };
-            $scope.selectedFeatures = function selectedFeatures(){
-                return filterFilter($scope.deviceFeatures, { selected: true });
-            };
-
-            //watchers
-            $scope.$watch('diseaseList|filter:{selected:true}', function (nv) {
-                $scope.diseaseSelection = nv.map(function (item) {
-                    return item.name;
-                });
-            }, true);
-            $scope.$watch('riskFactors|filter:{selected:true}', function (nv) {
-                $scope.riskSelection = nv.map(function (item) {
-                    return item.name;
-                });
-            }, true);
-            $scope.$watch('otherRisks|filter:{selected:true}', function (nv) {
-                $scope.riskSelection = nv.map(function (item) {
-                    return item.name;
-                });
-            }, true);
-            $scope.$watch('deviceFeatures|filter:{selected:true}', function (nv) {
-                $scope.featureSelection = nv.map(function (item) {
-                    return item.name;
-                });
-            }, true);
-
-            $scope.isValidateStep1 = function(val) {
-                $scope.diseaseSelection = val;
-
-                if($scope.diseaseSelection.length == 0){
-                    //making default selection to be HF for multiselect
-                    //$scope.diseaseSelection = ["Heart Failure"];
-                    //making default selection to be HF for single select
-                    $scope.diseaseSelection = "Heart Failure";
-                }
->>>>>>> 9e0fef27fa6f7ab2c9477c8a67df756e025f211c
+                $scope.deviceCategory = [{name:"Body Composition Analyzer",imgsrc:""},
+                    {name:"Blood Pressure Monitor",imgsrc:""},
+                    {name:"Activity Tracker",imgsrc:""},
+                    {name:"Heart Rate Monitor",imgsrc:""},
+                    {name:"Oximeter",imgsrc:""}];
 
                 //assigning image source to device category
-                for (var item in $scope.deviceCategory) {
-                    $scope.deviceCategory[item].imgsrc = "../images/device-category/" + $scope.deviceCategory[item].name.replaceAll(" ", "") + ".png";
+                for(var item in $scope.deviceCategory){
+                    $scope.deviceCategory[item].imgsrc = "../images/device-category/"+$scope.deviceCategory[item].name.replaceAll(" ","")+".png";
                 }
 
                 //filter selection
                 $scope.selectedDiseases = function selectedDiseases() {
-                    return filterFilter($scope.diseaseList, {selected: true});
+                    return filterFilter($scope.diseaseList, { selected: true });
                 };
-                $scope.selectedRisks = function selectedRisks() {
-                    return filterFilter($scope.riskFactors, {selected: true});
+                $scope.selectedRisks = function selectedRisks(){
+                    return filterFilter($scope.riskFactors, { selected: true });
                 };
-                $scope.selectedFeatures = function selectedFeatures() {
-                    return filterFilter($scope.deviceFeatures, {selected: true});
+                $scope.selectedFeatures = function selectedFeatures(){
+                    return filterFilter($scope.deviceFeatures, { selected: true });
                 };
 
                 //watchers
@@ -412,130 +311,134 @@
                     });
                 }, true);
 
-<<<<<<< HEAD
-                $scope.isValidateStep1 = function (val) {
+                $scope.isValidateStep1 = function(val) {
                     $scope.diseaseSelection = val;
 
-                    if ($scope.diseaseSelection.length == 0) {
+                    if($scope.diseaseSelection.length == 0){
                         //making default selection to be HF for multiselect
                         //$scope.diseaseSelection = ["Heart Failure"];
                         //making default selection to be HF for single select
                         $scope.diseaseSelection = "Heart Failure";
-=======
-                $scope.riskFactors = convertArrToJson(rf);
-                $scope.otherRisks = convertArrToJson(_.difference(rfAll,rf));
-            };
-
-            // function to validate risk factor selection
-            $scope.isValidateStep2 = function(){
-                // $scope.riskSelection contains the selected risk factors
-            };
-
-            // function to validate device category
-            $scope.isValidateStep3 = function(devCat){
-                $scope.devCat = devCat;
-            };
-
-            $scope.addUnusualSymptom = function(){
-                if($scope.wizard.symptom != '' && !angular.isUndefined($scope.wizard.symptom)){
-                    $scope.unusualList.push($scope.wizard.symptom);
-                    $scope.wizard.symptom = '';
-                    $scope.wizard.inpError = false;
-                }
-                else{
-                    $scope.wizard.inpError = true;
-                }
-            };
->>>>>>> 9e0fef27fa6f7ab2c9477c8a67df756e025f211c
-
                     }
 
                     //pull from database risk factors for each disease using a service call or find an optimization
-                    if ($scope.diseaseSelection.indexOf("Heart Failure") != -1) {
+                    if($scope.diseaseSelection.indexOf("Heart Failure") != -1){
                         rf = ["Weight", "BMI", "Resting Heart Rate", "Blood Pressure", "Oxygen Saturation", "Activity"];
                     }
-                    if ($scope.diseaseSelection.indexOf("COPD") != -1) {
-                        rf = ["Forced Expiratory Volume", "Peak Expiratory Flow", "Ventilation-Perfusion Ratio", "Oxygen Saturation", "Tobacco Cessation", "Exercise/Activity"];
+                    if($scope.diseaseSelection.indexOf("COPD") != -1){
+                        rf = ["Forced Expiratory Volume", "Peak Expiratory Flow", "Ventilation-Perfusion Ratio","Oxygen Saturation","Tobacco Cessation","Exercise/Activity"];
 
                     }
-                    if ($scope.diseaseSelection.indexOf("Atrial Fibrillation") != -1) {
-                        rf = ["Heart Rate", "Heart Rhythm"];
+                    if($scope.diseaseSelection.indexOf("Atrial Fibrillation") != -1){
+                        rf = ["Heart Rate","Heart Rhythm"];
                     }
-                    if ($scope.diseaseSelection.indexOf("Diabetes") != -1) {
-                        rf = ["Blood Glucose", "Weight", "Activity/Exercise", "HbA1c"];
+                    if($scope.diseaseSelection.indexOf("Diabetes") != -1){
+                        rf = ["Blood Glucose","Weight","Activity/Exercise","HbA1c"];
                     }
 
-<<<<<<< HEAD
                     $scope.riskFactors = convertArrToJson(rf);
-                    $scope.otherRisks = convertArrToJson(_.difference(rfAll, rf));
+                    $scope.otherRisks = convertArrToJson(_.difference(rfAll,rf));
                 };
 
                 // function to validate risk factor selection
-                $scope.isValidateStep2 = function () {
+                $scope.isValidateStep2 = function(){
                     // $scope.riskSelection contains the selected risk factors
                 };
 
                 // function to validate device category
-                $scope.isValidateStep3 = function (devCat) {
+                $scope.isValidateStep3 = function(devCat){
                     $scope.devCat = devCat;
                 };
 
-                $scope.addUnusualSymptom = function () {
-                    if ($scope.wizard.symptom != '' && !angular.isUndefined($scope.wizard.symptom)) {
+                $scope.addUnusualSymptom = function(){
+                    if($scope.wizard.symptom != '' && !angular.isUndefined($scope.wizard.symptom)){
                         $scope.unusualList.push($scope.wizard.symptom);
                         $scope.wizard.symptom = '';
                         $scope.wizard.inpError = false;
                     }
-                    else {
+                    else{
                         $scope.wizard.inpError = true;
                     }
-
                 };
 
+                $scope.notify = function(type) {
 
+                    switch (type) {
+                        case 'success':
+                            $timeout(function(){
+                                $location.path("/");
 
+                                // reset image paths on finish
+                                for(var item in $scope.deviceCategory){
+                                    $scope.deviceCategory[item].imgsrc = "../images/device-category/"+$scope.deviceCategory[item].name.replaceAll(" ","")+".png";
+                                }
+                            },2000);
+                            return logger.logSuccess("Thank You. Your device recommendations have been sent to the patient");
+                        case 'warning':
+                            return logger.logWarning("Warning!");
+                        case 'error':
+                            return logger.logError("Oh snap! Change a few things up and try submitting again.");
+                    }
+                };
 
-
-
-                    $scope.notify = function (type) {
-
-                        switch (type) {
-                            case 'success':
-                                $timeout(function () {
-                                    $location.path("/");
-
-                                    // reset image paths on finish
-                                    for (var item in $scope.deviceCategory) {
-                                        $scope.deviceCategory[item].imgsrc = "../images/device-category/" + $scope.deviceCategory[item].name.replaceAll(" ", "") + ".png";
-                                    }
-                                }, 2000);
-                                return logger.logSuccess("Thank You. Your device recommendations have been sent to the patient");
-                            case 'warning':
-                                return logger.logWarning("Warning!");
-                            case 'error':
-                                return logger.logError("Oh snap! Change a few things up and try submitting again.");
-
+                $scope.addDevice = function(devCat,devSrc){
+                    for(var val in $scope.deviceCategory){
+                        if(devCat == $scope.deviceCategory[val].name){
+                            $scope.deviceCategory[val].imgsrc = devSrc;
                         }
-
-                    }
-                        return $scope.finishedWizard = function () {
-                            //find device call
-                            alert("Thank You. Subscription has been made to the patient");
-                        };
-                    }
-
-
-                ]);
-
-=======
-            $scope.addDevice = function(devCat,devSrc){
-                for(var val in $scope.deviceCategory){
-                    if(devCat == $scope.deviceCategory[val].name){
-                        $scope.deviceCategory[val].imgsrc = devSrc;
                     }
                 }
+
+                return $scope.finishedWizard = function() {
+                    //find device call
+                    alert("Thank You. Subscription has been made to the patient");
+                };
             }
->>>>>>> 9e0fef27fa6f7ab2c9477c8a67df756e025f211c
+        ]);
+
+    //angular.module('app.ui.form').factory("DiseaseFactory", function($scope, DiseaseFactory) {
+    /* var disease = function(diseaselist){
+     this.initialize = function(){
+     var url = "http://localhost:9000/mongoapi/getAllDiseases/";
+     console.log(url);
+     var diseaseData = $http.jsonp(url);
+     var self = this;
+     diseaseData.then(function(response){
+     angular.extend(self,response.data);
+     });
+     };
+     this.initialize();
+     };
+     return (disease);
+     });*/
+
+    /*var urlBase = 'http://localhost:9000/';
+
+     var dataFactory = {};
+
+     dataFactory.getDiseases = function () {
+     return $http.get(urlBase + 'mongoapi/getAllDiseases');
+     };
+     return dataFactory;
+     });*/
+
+
+    /*angular.module('app.ui.form').factory('DiseaseFactory', function($q, $http){
+     return{
+     getDiseases:function(){
+     var deferred = $q.defer(),
+     httpPromise = $http.get('http://localhost:9000/mongoapi/getAllDiseases/');
+
+     httpPromise.then(function(response) {
+     deferred.resolve(response);
+     }, function(error){
+     console.error(error);
+     });
+     return deferred.promise;
+
+     }
+     };
+     });*/
 
 })();
 
